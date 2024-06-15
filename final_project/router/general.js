848a2d200d12737db6d955f510fc17377ba77a1f
Hrithik <hrithikkrishna100@gmail.com>
2024-06-15 06:40:10 -0400

    Task 6 complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,25 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => { 
+    return users.some(user => user.username == username);
+}
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {username, password} = req.body;
+
+  if(!username || !password) {
+    return res.status(400).json({message: "Username and password required"});
+    } 
+
+  if(doesExist(username)) {
+    return res.status(409).json({message: "User already exists"})
+    }
+
+    users.push({"username":username, "password":password}); 
+    return res.status(200).json({message: "User successfully registered!"})
+
 });
 
 // Get the book list available in the shop
