c57a1a319f2a0a74b6b7b9010a238cb501ca2c54
Hrithik <hrithikkrishna100>
2024-06-19 06:20:16 -0400

    Update and authenticate users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,53 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    let validUsers = users.some(user=> {return user.username === username})
+
+    if(validUsers) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    let authUsers = users.some(user=> {
+       return user.username === username && user.password == password 
+    })
+
+    if(authUsers) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(!username || !password) {
+        return res.status(404).json({message: "Error logging in"})
+    };
+
+    console.log("Login error")
+
+    if(authenticatedUser(username,password)) {
+        let accessToken = jwt.sign({data : password}, 
+        'access', 
+        {expiresIn : 60*60 });
+    
+        req.session.authorization = {accessToken, username}
+
+        return res.status(200).json({message: "User successfully logged in"})
+    } else {
+        return res.status(404).json({message: "Error logging in"})
+    }
 });
 
 // Add a book review
