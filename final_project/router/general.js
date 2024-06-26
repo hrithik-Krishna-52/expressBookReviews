be24fefc5205ceaf3e33016383d053a1e1711d62
Hrithk <Hrithk>
2024-06-26 11:50:28 -0400

    Final Task done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,7 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+
 const doesExist = (username) => { 
     return users.some(user => user.username == username);
 }
@@ -25,51 +26,112 @@ public_users.post("/register", (req,res) => {
 
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
+const fetchBooks = async ()=> {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve(books)
+        },1000);
+    });
+};
+
+// Get the book list available in the shop      
+public_users.get('/',async function (req, res) {
   //Write your code here
-  return res.status(300).send(JSON.stringify(books));
+  try {
+  const booksData = await fetchBooks();
+  return res.status(300).json(booksData);
+} catch(error) {
+    console.error('Error fetching books', error)
+    return res.status(500).send('Internal server error');
+} 
 });
+
+const fetchDetails = async (isbn) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            if(books[isbn]) {
+                resolve(books[isbn])
+            } else {
+                reject(new Error('Book not found'))
+            }
+        },1000)
+    });
+}
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
-  return res.status(300).send(JSON.stringify(books[req.params.isbn]));
+    try {
+        const isbn = req.params.isbn;
+        const bookDetails = await fetchDetails(isbn);
+        res.status(200).json(bookDetails);
+    } catch(error) {
+        console.error('Error fetching book isbn',error)
+        res.status(500).send('Internal Server Error');
+    }
  });
   
+const fetchBasedOnAuthor = async(author) =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            const matchingBooks = [];
+            for(const [key, book] of Object.entries(books)) {
+                if(book.author.toLowerCase() == author.toLowerCase()) {
+                    matchingBooks.push({ id: key, ...book });
+                }
+            }
+
+            if(matchingBooks.length > 0) {
+                resolve(matchingBooks);
+            } else {
+                reject(new Error('No books found for the given Author'))
+            }
+        },1000);
+    });
+};
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-const author = req.params.author.toLowerCase();
-const matchingBooks = [];
-
-  Object.values(books).forEach(book => {
-    if(book.author.toLowerCase() == author ) {
-        matchingBooks.push(book);
+public_users.get('/author/:author', async function (req, res) {
+ try { 
+      const author = req.params.author;
+      const booksFromAuthor = await fetchBasedOnAuthor(author);
+      res.status(200).json(booksFromAuthor);
+    } catch(error) {
+        console.error('Error in fetching author', error);
+        res.status(500).send('Internal Server Error');
     }
-  });
-
-  if(matchingBooks.length === 0 ) {
-    return res.status(404).send("No matching book found")
-  };
-
-  return res.json(matchingBooks);
 });
 
+//Async function
+const fetchBookBasedOnTitle = async (title) => {
+    return new Promise((resolve, reject)=> {
+        setTimeout(() => {  
+            const matchingBooks = [];
 
+            for(const [key, book] of Object.entries(books)) {
+                if(book.title.toLowerCase() == title.toLowerCase()) {
+                    matchingBooks.push({id: key, ...book})
+                }
+            }
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-    const title = req.params.title.toLowerCase();
-
-    Object.values(books).forEach(book => {
-        if(book.title.toLowerCase() == title) {
-            return res.send(book);
-        }
+            if(matchingBooks.length > 0) {
+                resolve(matchingBooks);
+            } else {
+                reject(new Error('No Books found for given title'))
+            }
+        }, 1000);
     })
+};
+// Get all books based on title
+public_users.get('/title/:title',async function (req, res) {
+try {
+    const title = req.params.title;
+    const booksWithTitle = await fetchBookBasedOnTitle(title);
+    res.status(200).send(booksWithTitle);
 
-    return res.status(404).json({error: "No matching title found"});
+    } catch(error) {
+        console.error('No matching book with Title found', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 
